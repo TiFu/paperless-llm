997dccaae5ca03c1588a70c6a597ed8706e8ddcf
Tino Fuhrmann <tino-fuhrmann@web.de>
2026-07-08 22:26:06 +0200

    feat: Allow any user to edit settings to circumvent limitations of paperless is_staff

--- server/src/web/AuthController.ts
+++ server/src/web/AuthController.ts
@@ -1,7 +1,6 @@
 import { AuthApplicationService } from '../application/AuthApplicationService.js';
 import { UserContext } from '../domain/auth/UserContext.js';
 import { UoWFactory } from '../infrastructure/UoW.js';
-import { SETTINGS_RESOURCE_ID } from '../domain/authorization/IPermissionsRepository.js';
 
 export class AuthController {
   constructor(
@@ -14,10 +13,6 @@ export class AuthController {
   }
 
   async me(user: UserContext): Promise<{ username: string; canEditSettings: boolean }> {
-    await using uow = await this.uowFactory.createUoW(user);
-    await uow.start();
-    const canEditSettings = await uow.getPermissions().hasPermission('settings', SETTINGS_RESOURCE_ID, user.username);
-    await uow.commit();
-    return { username: user.username, canEditSettings };
+    return { username: user.username, canEditSettings: true };
   }
 }
